a76d207b90c59ceab733cde74b17f34627fcae14
hatch <hatch.tech.eng@gmail.com>
2020-04-19 14:28:04 +0900

    AddScript

---
+++ EditorMoveDirectory.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+ 
+public static class EditorMoveDirectory
+{
+	[MenuItem("Assets/Move Directory")]
+	private static void MoveAssets()
+	{
+		var guids = Selection.assetGUIDs;
+		if (guids.Length <= 0)
+			return;
+ 
+		var path = EditorUtility.OpenFolderPanel("Select Move Directory", "Assets", "");
+		if (string.IsNullOrEmpty(path))
+			return;
+ 
+		var inProjectPath = path.Replace(Application.dataPath, "Assets");
+		foreach (var guid in guids)
+		{
+			var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+			var fileName = System.IO.Path.GetFileName(assetPath);
+			AssetDatabase.MoveAsset(assetPath, System.IO.Path.Combine(inProjectPath, fileName));
+		}
+	}
+}
